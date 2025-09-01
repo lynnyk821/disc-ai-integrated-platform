da350732d576bd29cb519297afd430facae59bc4
lynnyk265 <lynnyk265@gmail.com>
2025-09-01 11:17:37 +0300

    change chat message repository

--- backend/src/main/java/ua/todolist/server/app/backend/database/repository/ChatMessageRepository.java
+++ backend/src/main/java/ua/todolist/server/app/backend/database/repository/ChatMessageRepository.java
@@ -3,8 +3,6 @@ package ua.todolist.server.app.backend.database.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.todolist.server.app.backend.database.entity.ChatMessageEntity;
 
-import java.util.List;
-
 public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Long> {
-    List<ChatMessageEntity> findTopNByOrderByCreatedAtDesc(int n);
+
 }
